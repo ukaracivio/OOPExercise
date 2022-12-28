5f2b2993a17463499df2650c48b33a74b6c818d8
khas <khas@KHASPC1214>
2022-12-28 14:29:27 +0300

    Add project files.

---
+++ OOPExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    // İnsan isimli bir sınıf içinde ad, soyad, boy, kilo,yaş alanlarını tanımlayıp, bir kurucu metot içinde bu alanlar için varsayılan değer atamalarını yapacağız. Son olarak da "Tanis" isimli metotla girilen bilgileri kullanıcıya belli ,güzel, açıklayıcı bir şekilde görüntüleyen bir programcik yazınız.
+
+    // Örnek : Ben Ümit KARAÇİVİ...Boyum 1.85 m, kilom 63 kg, yaşım 58'dir.
+
+
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
